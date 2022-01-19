c2645be9cd0bc1f93686e0786361ed40b28cb16f
Jasmine <jazicorndev@gmail.com>
2022-01-19 10:52:24 -0500

    added exception to main file

--- src/main/java/DragonCave/project/Main.java
+++ src/main/java/DragonCave/project/Main.java
@@ -1,23 +1,16 @@
 package DragonCave.project;
 
 import DragonCave.project.actions.Events;
-import DragonCave.project.actions.Scripts;
 
 public class Main {
 
-    public static void run() {
-        Scripts.Story story = new Scripts.Story();
-        story.start();
-
-        Events events = new Events();
-        events.play();
-        events.quit();
-    }
-
     public static void main(String[] args) {
         Events events = new Events();
-        do {
-            run();
-        } while(!events.quit || !events.playAgain);
+        try {
+            events.play();
+        }
+        catch(NullPointerException e) {
+            System.out.println("Event Object Not Found");
+        }
     }
 }
